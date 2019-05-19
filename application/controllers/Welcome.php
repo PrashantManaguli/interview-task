ea7a192342d3aa9f1980d19f50c0812232c88231
unknown <prashantmanaguli@gmail.com>
2019-05-18 20:54:54 -0700

    model removed

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,7 @@ class Welcome extends CI_Controller {
 
 	 function __construct() {
         parent::__construct();
-        $this->load->model("main_model");
+       
     }
 	 
 	public function index()
