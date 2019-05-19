ff2eb65b3a90ef1c9990993769dbc4103db8d072
unknown <prashantmanaguli@gmail.com>
2019-05-18 23:01:24 -0700

    payment added

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,7 +8,7 @@
 	</style>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://js.instamojo.com/v1/checkout.js"></script>
-   <script src='https://www.google.com/recaptcha/api.js'></script>
+   
    <script src="https://www.google.com/recaptcha/api.js?render=6LeRSqQUAAAAAD7P0dJo4t9D0VDdWBCqqFGIXSGc"></script>
   <script>
   grecaptcha.ready(function() {
